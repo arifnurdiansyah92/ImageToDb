27f1d3abb8ab9e50302a3ad0dddf2b9818c5b393
Arif Nurdiansyah <arifnurdiansyah92@hotmail.com>
2018-07-31 11:09:22 +0700

    Save Image to Database

---
+++ ImageToDb/Migrations/20180731040007_init.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ImageToDb.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Image",
+                columns: table => new
+                {
+                    ImageId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Img = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Image", x => x.ImageId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Image");
+        }
+    }
+}
--- ImageToDb/Startup.cs
+++ ImageToDb/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ImageToDb.Models;
 
 namespace ImageToDb
 {
@@ -22,6 +24,9 @@ namespace ImageToDb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<ImageToDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ImageToDbContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
